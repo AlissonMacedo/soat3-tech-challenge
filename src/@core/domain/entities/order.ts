9a328e12ba48fac3bb4171655ce1292cfbcfbab2
Alisson Macedo <alissontecnico@gmail.com>
2025-03-30 21:16:56 -0300

    create customer product categories and struture DDD

---
+++ src/@core/domain/entities/order.ts
@@ -0,0 +1,11 @@
+export class Order {
+  id: string;
+  status: string;
+  created_at: Date;
+  updated_at: Date;
+
+  constructor(id: string, status: string) {
+    this.id = id;
+    this.status = status;
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, NestModule } from '@nestjs/common';
+import { CustomersModule } from './custumers/custumers.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { DatabaseModule } from './adapter/driven/infra/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { CategoriesModule } from './@core/categories.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CustomersModule,
+    CategoriesModule,
+    ProductsModule,
+    OrdersModule,
+    DatabaseModule,
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
